af29467182893ac7d4256af166354e96a0314840
Sebastián Solorzano <sol21826@uvg.edu.gt>
2024-09-13 17:18:15 -0600

    Inicio portafolio

---
+++ portafolio/src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "@mdi/font/css/materialdesignicons.css";
+import './index.css';
+import App from './App';
+
+// https://ui.aceternity.com/
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
