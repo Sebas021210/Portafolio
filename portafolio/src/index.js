5add1a5027041b9dde668602ec002b242bedb781
Sebastián Solorzano <sol21826@uvg.edu.gt>
2024-09-18 01:40:19 -0600

    Modificaciones

--- portafolio/src/index.js
+++ portafolio/src/index.js
@@ -2,10 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import "bootstrap/dist/css/bootstrap.min.css";
 import "@mdi/font/css/materialdesignicons.css";
+import 'primereact/resources/themes/saga-blue/theme.css';
+import 'primereact/resources/primereact.min.css';
 import './index.css';
 import App from './App';
 
 // https://ui.aceternity.com/
+// https://primereact.org/
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
