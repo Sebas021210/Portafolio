df20e1697ddf987ef13fa56f561b2fdef2ac13d3
Sebastián Solorzano <sol21826@uvg.edu.gt>
2024-09-18 17:03:26 -0600

    Inicio home

--- portafolio/src/App.js
+++ portafolio/src/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
+import Home from './components/home/home';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>My Portfolio</h1>
-      </header>
+      <Home />
     </div>
   );
 }
---
+++ portafolio/src/components/home/home.js
@@ -0,0 +1,21 @@
+import React from "react";
+import './home.css';
+
+function Home() {
+    return (
+        <div className="home">
+            <div className="lines">
+                <div className="line"></div>
+                <div className="line"></div>
+                <div className="line"></div>
+            </div>
+            <div className="content">
+                <div className="name">
+                    <h1>Sebastián José Solorzano Pérez</h1>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Home;
