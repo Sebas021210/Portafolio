940732a11442862af0dcbc6647f6796b791d9e40
Sebastián Solorzano <sol21826@uvg.edu.gt>
2024-09-18 19:29:39 -0600

    Home

--- portafolio/src/components/home/home.js
+++ portafolio/src/components/home/home.js
@@ -1,4 +1,8 @@
 import React from "react";
+import Icon from '@mdi/react';
+import { mdiGithub } from '@mdi/js';
+import { mdiLinkedin } from '@mdi/js';
+import { mdiInstagram } from '@mdi/js';
 import './home.css';
 
 function Home() {
@@ -11,7 +15,15 @@ function Home() {
             </div>
             <div className="content">
                 <div className="name">
-                    <h1>Sebastián José Solorzano Pérez</h1>
+                    <h1>Sebastián Solorzano</h1>
+                </div>
+                <div className="description">
+                    <h2>Student of Computer Science</h2>
+                </div>
+                <div className="contacts">
+                    <a href="https://github.com/Sebas021210" target="_blank" rel="noopener noreferrer"><Icon path={mdiGithub} size={2} color="white" /></a>
+                    <a href="https://www.linkedin.com/in/sebasti%C3%A1n-jos%C3%A9-solorzano-p%C3%A9rez-7544b9205/" target="_blank" rel="noopener noreferrer"><Icon path={mdiLinkedin} size={2} color="white" /></a>
+                    <a href="https://www.instagram.com/sebass.sp/" target="_blank" rel="noopener noreferrer"><Icon path={mdiInstagram} size={2} color="white" /></a>
                 </div>
             </div>
         </div>
