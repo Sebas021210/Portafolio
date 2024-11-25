02eb884dd17d9a3bb64f0e30a49fb6fcacaa2484
Sebastián Solorzano <sol21826@uvg.edu.gt>
2024-11-25 17:34:03 -0600

    Diseño About Me
    Diseño y Responsive

--- portafolio/src/components/About/About.js
+++ portafolio/src/components/About/About.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { Fieldset } from 'primereact/fieldset';
+import SebasSol from '../../assets/SebasSol.JPEG';
 import './About.css';
 
 function About() {
@@ -9,9 +11,46 @@ function About() {
                     <h1>About Me</h1>
                 </div>
                 <div className="about-description">
-                    <p>
-                        I am a student of Computer Science at the University of Costa Rica. I am passionate about technology and software development. I am always looking for new challenges and opportunities to learn. I am currently working as a software developer intern at the company Aceternity.
-                    </p>
+                    <div className="about-description-info">
+                        <div className="about-description-info-image">
+                            <img src={SebasSol} alt="SebasSol" className="profile-image" />
+                        </div>
+                        <div className="about-description-info-text">
+                            <p>
+                                Hi! I'm Sebastián Solorzano, a student of Computer Science at the National University of Costa Rica. I am passionate about software development and I am always looking for new challenges to learn and grow as a professional. I have experience in web development and desktop application development. I am currently learning about cloud computing and cybersecurity.
+                            </p>
+                        </div>
+                    </div>
+                    <div className="about-description-tec">
+                        <h2>Technologies</h2>
+                        <div className="about-description-tec-info">
+                            <Fieldset legend="Languages" className="customFieldset">
+                                <p className="m-0">
+                                    Java, Python, C, C++, JavaScript
+                                </p>
+                            </Fieldset>
+                            <Fieldset legend="Front-End" className="customFieldset">
+                                <p className="m-0">
+                                    HTML, CSS, React, Angular, Vue, Bootstrap
+                                </p>
+                            </Fieldset>
+                            <Fieldset legend="DataBase" className="customFieldset">
+                                <p className="m-0">
+                                    PostgreSQL, MongoDB, Neo4j
+                                </p>
+                            </Fieldset>
+                            <Fieldset legend="Back-End" className="customFieldset">
+                                <p className="m-0">
+                                    Node.js, Express
+                                </p>
+                            </Fieldset>
+                            <Fieldset legend="Tools" className="customFieldset">
+                                <p className="m-0">
+                                    Git, Docker, AWS, Firebase
+                                </p>
+                            </Fieldset>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
--- portafolio/src/index.js
+++ portafolio/src/index.js
@@ -4,6 +4,8 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "@mdi/font/css/materialdesignicons.css";
 import 'primereact/resources/themes/saga-blue/theme.css';
 import 'primereact/resources/primereact.min.css';
+import 'primeicons/primeicons.css';
+import 'primeflex/primeflex.css';
 import './index.css';
 import App from './App';
 
