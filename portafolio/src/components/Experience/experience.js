fce0ae8c2dd5bf1ec2d15513730697b0f60dda31
Sebastián Solorzano <sol21826@uvg.edu.gt>
2024-11-26 20:56:10 -0600

    Diseño de experiencia
    Línea del tiempo creada

--- portafolio/src/components/Experience/experience.js
+++ portafolio/src/components/Experience/experience.js
@@ -1,4 +1,14 @@
 import React from "react";
+import Timeline from '@mui/lab/Timeline';
+import TimelineItem from '@mui/lab/TimelineItem';
+import TimelineSeparator from '@mui/lab/TimelineSeparator';
+import TimelineConnector from '@mui/lab/TimelineConnector';
+import TimelineContent from '@mui/lab/TimelineContent';
+import TimelineOppositeContent from '@mui/lab/TimelineOppositeContent';
+import TimelineDot from '@mui/lab/TimelineDot';
+import LaptopMacIcon from '@mui/icons-material/LaptopMac';
+import SchoolIcon from '@mui/icons-material/School';
+import Typography from '@mui/material/Typography';
 import './experience.css';
 
 function Experience() {
@@ -8,18 +18,126 @@ function Experience() {
                 <h1>Experience</h1>
             </div>
             <div className="experience-content">
-                <div className="experience-item">
-                    <h2>Web Developer</h2>
-                    <h3>Freelancer</h3>
-                    <h4>2021 - Present</h4>
-                    <p>Developing web applications using React.js, Node.js, Express.js, and MongoDB.</p>
-                </div>
-                <div className="experience-item">
-                    <h2>Software Developer</h2>
-                    <h3>Freelancer</h3>
-                    <h4>2021 - Present</h4>
-                    <p>Developing desktop applications using Java and Python.</p>
-                </div>
+                <Timeline position="alternate">
+                    <TimelineItem>
+                        <TimelineOppositeContent
+                            sx={{ m: 'auto 0' }}
+                            align="right"
+                            variant="body2"
+                            color="text.secondary"
+                        >
+                            2007 - 2020
+                        </TimelineOppositeContent>
+                        <TimelineSeparator>
+                            <TimelineConnector />
+                            <TimelineDot >
+                                <SchoolIcon />
+                            </TimelineDot>
+                            <TimelineConnector />
+                        </TimelineSeparator>
+                        <TimelineContent sx={{ py: '12px', px: 2 }}>
+                            <Typography variant="h6" component="span">
+                                High School
+                            </Typography>
+                            <Typography>Because you need strength</Typography>
+                        </TimelineContent>
+                    </TimelineItem>
+
+                    <TimelineItem>
+                        <TimelineOppositeContent
+                            sx={{ m: 'auto 0' }}
+                            variant="body2"
+                            color="text.secondary"
+                        >
+                            2021 - 2025
+                        </TimelineOppositeContent>
+                        <TimelineSeparator>
+                            <TimelineConnector />
+                            <TimelineDot style={{ backgroundColor: "#598392" }}>
+                                <SchoolIcon />
+                            </TimelineDot>
+                            <TimelineConnector />
+                        </TimelineSeparator>
+                        <TimelineContent sx={{ py: '12px', px: 2 }}>
+                            <Typography variant="h6" component="span">
+                                University
+                            </Typography>
+                            <Typography>Because it&apos;s awesome!</Typography>
+                        </TimelineContent>
+                    </TimelineItem>
+
+                    <TimelineItem>
+                        <TimelineOppositeContent
+                            sx={{ m: 'auto 0' }}
+                            align="right"
+                            variant="body2"
+                            color="text.secondary"
+                        >
+                            2021 - 2023
+                        </TimelineOppositeContent>
+                        <TimelineSeparator>
+                            <TimelineConnector />
+                            <TimelineDot>
+                                <LaptopMacIcon />
+                            </TimelineDot>
+                            <TimelineConnector />
+                        </TimelineSeparator>
+                        <TimelineContent sx={{ py: '12px', px: 2 }}>
+                            <Typography variant="h6" component="span">
+                                Teacher at a Computer Academy
+                            </Typography>
+                            <Typography>Because you need strength</Typography>
+                        </TimelineContent>
+                    </TimelineItem>
+
+                    <TimelineItem>
+                        <TimelineOppositeContent
+                            sx={{ m: 'auto 0' }}
+                            variant="body2"
+                            color="text.secondary"
+                        >
+                            2024 - 2025
+                        </TimelineOppositeContent>
+                        <TimelineSeparator>
+                            <TimelineConnector />
+                            <TimelineDot style={{ backgroundColor: "#598392" }}>
+                                <LaptopMacIcon />
+                            </TimelineDot>
+                            <TimelineConnector />
+                        </TimelineSeparator>
+                        <TimelineContent sx={{ py: '12px', px: 2 }}>
+                            <Typography variant="h6" component="span">
+                                Teaching Assistant at the University
+                            </Typography>
+                            <Typography>Because it&apos;s awesome!</Typography>
+                        </TimelineContent>
+                    </TimelineItem>
+
+                    <TimelineItem>
+                        <TimelineOppositeContent
+                            sx={{ m: 'auto 0' }}
+                            align="right"
+                            variant="body2"
+                            color="text.secondary"
+                        >
+                            2024 - 2025
+                        </TimelineOppositeContent>
+                        <TimelineSeparator>
+                            <TimelineConnector />
+                            <TimelineDot>
+                                <LaptopMacIcon />
+                            </TimelineDot>
+                            <TimelineConnector />
+                        </TimelineSeparator>
+                        <TimelineContent sx={{ py: '12px', px: 2 }}>
+                            <Typography variant="h6" component="span">
+                                Teaching Assistant at the University
+                            </Typography>
+                            <Typography>Because you need strength</Typography>
+                        </TimelineContent>
+                    </TimelineItem>
+
+                </Timeline>
             </div>
         </div>
     );
