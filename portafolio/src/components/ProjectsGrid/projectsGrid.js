4da62718fed99bc85fb4578e4f9f2b74ee5e1a95
Sebastián Solorzano <sol21826@uvg.edu.gt>
2024-11-29 01:16:57 -0600

    Avances en el diseño proyectos
    Diseño base

--- portafolio/src/components/ProjectsGrid/projectsGrid.js
+++ portafolio/src/components/ProjectsGrid/projectsGrid.js
@@ -1,17 +1,27 @@
 import React from "react";
-import SebasSol from '../../assets/SebasSol.JPEG';
+import img1 from "../../assets/img1.png"
+import Icon from '@mdi/react';
+import { mdiGithub, mdiOpenInNew } from '@mdi/js';
 import './projectsGrid.css';
 
 function ProjectsGrid() {
     return (
         <div className="projects-grid">
             <div className="projects-grid-img">
-                <img src={SebasSol} alt="SebasSol" />
+                <img src={img1} alt="img1" />
             </div>
             <div className="projects-grid-content">
-                <h1>Project</h1>
+                <h1>Nasa Space App</h1>
                 <div className="projects-grid-description">
-                    <p>Description</p>
+                    <p>Having struggled with understanding how the Spotify OAuth flow works, I made the course I wish I could have had. Unlike tutorials that only cover a few concepts and leave you with half-baked GitHub repositories, this course covers everything from explaining the principles of REST APIs to implementing Spotify's OAuth flow and fetching API data in a React app.</p>
+                </div>
+                <div className="projects-grid-tech">
+                    <p style={{ color: "#000" }} >React, Node.js, Express, Spotify API</p>
+                </div>
+                <div className="projects-grid-buttons">
+                    <a href="https://github.com/Sebas021210" target="_blank" rel="noopener noreferrer"><Icon path={mdiGithub} size={1.5} color="black" /></a>
+                    <a href="https://github.com/Sebas021210" target="_blank" rel="noopener noreferrer"><Icon path={mdiOpenInNew} size={1.5} color="black" /></a>
+                    
                 </div>
             </div>
         </div>
