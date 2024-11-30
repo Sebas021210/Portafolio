6cc1a81d4f085322fd4b5abe574659836f92075a
Sebastián Solorzano <sol21826@uvg.edu.gt>
2024-11-30 00:36:22 -0600

    Proyectos agregados
    Información de los proyectos

--- portafolio/src/components/Projects/projects.js
+++ portafolio/src/components/Projects/projects.js
@@ -1,5 +1,10 @@
 import React from "react";
 import ProjectsGrid from "../ProjectsGrid/projectsGrid";
+import Nasa from "../../assets/Nasa.png";
+import DisneyClone from "../../assets/Disney+Clone.png";
+import WhastApp from "../../assets/WhatsApp.png";
+import Portfolio from "../../assets/Portfolio.png";
+import ImageCSS from "../../assets/ImageCSS.png";
 import './projects.css';
 
 function Projects() {
@@ -7,31 +12,51 @@ function Projects() {
         {
             id: 1,
             title: "Nasa Space App",
-            description: "Having struggled with understanding how the Spotify OAuth flow works, I made the course I wish I could have had. Unlike tutorials that only cover a few concepts and leave you with half-baked GitHub repositories, this course covers everything from explaining the principles of REST APIs to implementing Spotify's OAuth flow and fetching API data in a React app.",
-            tech: "React, Node.js, Express, Spotify API",
-            github: "",
-            demo: "",
-            img: "",
+            description: "This project was developed as part of a hackathon organized by NASA, where we were provided with official resources and space-related data. My team worked on designing and developing an innovative solution to address one of the challenges posed by the organization. We created a functional application to analyze and visualize the challenge's information.",
+            tech: "React, Node.js, NASA API",
+            github: "https://github.com/Jskenpo/imagine-our-connected-earth",
+            demo: "https://imagine-our-connected-earth.web.app/",
+            img: Nasa,
             reverse: false,
         },
         {
             id: 2,
-            title: "Nasa Space App",
-            description: "Having struggled with understanding how the Spotify OAuth flow works, I made the course I wish I could have had. Unlike tutorials that only cover a few concepts and leave you with half-baked GitHub repositories, this course covers everything from explaining the principles of REST APIs to implementing Spotify's OAuth flow and fetching API data in a React app.",
-            tech: "React, Node.js, Express, Spotify API",
-            github: "",
-            demo: "",
-            img: "",
+            title: "Disney+ Clone",
+            description: "This project is a clone of the Disney+ platform that replicates its design, including some of its movies, series, and content organized by categories, as well as the original page's animations. During development, I focused my efforts on creating a responsive interface capable of adapting to different screen sizes.",
+            tech: "React, Webapck, AWS",
+            github: "https://github.com/Sebas021210/Proyecto1-STW",
+            demo: "https://proyecto1-stw-8c95b.web.app/",
+            img: DisneyClone,
             reverse: true,
         },
         {
             id: 3,
-            title: "Nasa Space App",
-            description: "Having struggled with understanding how the Spotify OAuth flow works, I made the course I wish I could have had. Unlike tutorials that only cover a few concepts and leave you with half-baked GitHub repositories, this course covers everything from explaining the principles of REST APIs to implementing Spotify's OAuth flow and fetching API data in a React app.",
-            tech: "React, Node.js, Express, Spotify API",
-            github: "",
-            demo: "",
-            img: "",
+            title: "Portfolio",
+            description: "This is my previous portfolio, designed to showcase my skills, projects, and professional experience in a clear and effective way. In this project, I combined modern technologies like React and Webpack to create a smooth and user-friendly experience. The design was carefully crafted to be responsive, ensuring proper visualization across various devices.",
+            tech: "React, Webpack, AWS",
+            github: "https://github.com/Sebas021210/ProyectoFinal-STW",
+            demo: "https://portafolio-stw-8ca0c.web.app/",
+            img: Portfolio,
+            reverse: true,
+        },
+        {
+            id: 4,
+            title: "Whatsapp Clone",
+            description: "This project is a WhatsApp clone that uses the XMPP protocol for real-time messaging. It includes individual chats, sending and receiving messages, contact management, and an interface inspired by the official app. This experience helped me explore the fundamentals of real-time communication and learn more about implementing network protocols.",
+            tech: "React, Webpack, XMPP, Firebase",
+            github: "https://github.com/Sebas021210/Proyecto1-Redes",
+            demo: "https://github.com/Sebas021210/Proyecto1-Redes",
+            img: WhastApp,
+            reverse: false,
+        },
+        {
+            id: 5,
+            title: "Image CSS",
+            description: "This project involved recreating an illustration using only CSS, without any external images. It was a technical challenge that allowed me to experiment with advanced CSS properties. Moreover, this project provided an excellent opportunity to refine my design skills, exploring how styles can be manipulated to achieve complex and striking visual results.",
+            tech: "HTML, CSS",
+            github: "https://github.com/Sebas021210/LabCSS",
+            demo: "https://github.com/Sebas021210/LabCSS",
+            img: ImageCSS,
             reverse: false,
         },
     ]
@@ -44,7 +69,16 @@ function Projects() {
                 </div>
                 <div className="projects-description">
                     {projects.map((project, index) => (
-                        <ProjectsGrid key={project.id} reverse={index % 2 !== 0} />
+                        <ProjectsGrid 
+                            key={project.id} 
+                            reverse={index % 2 !== 0} 
+                            title={project.title}
+                            description={project.description}
+                            tech={project.tech}
+                            github={project.github}
+                            demo={project.demo}
+                            img={project.img}
+                        />
                     ))}
                 </div>
             </div>
--- portafolio/src/components/ProjectsGrid/projectsGrid.js
+++ portafolio/src/components/ProjectsGrid/projectsGrid.js
@@ -1,28 +1,27 @@
 import React from "react";
-import img1 from "../../assets/img1.png"
 import Icon from '@mdi/react';
 import { mdiGithub, mdiOpenInNew } from '@mdi/js';
 import './projectsGrid.css';
 
-function ProjectsGrid({ reverse }) {
+function ProjectsGrid({ reverse, title, description, tech, github, demo, img }) {
     return (
         <div className={`projects-grid ${reverse ? "reverse" : ""}`}>
             <div className="projects-grid-img">
-                <img src={img1} alt="img1" />
+                <img src={img} alt="img1" />
             </div>
             <div className="projects-grid-content">
-                <h1>Nasa Space App</h1>
+                <h1>{title}</h1>
                 <div className="projects-grid-description">
-                    <p>Having struggled with understanding how the Spotify OAuth flow works, I made the course I wish I could have had. Unlike tutorials that only cover a few concepts and leave you with half-baked GitHub repositories, this course covers everything from explaining the principles of REST APIs to implementing Spotify's OAuth flow and fetching API data in a React app.</p>
+                    <p>{description}</p>
                 </div>
                 <div className="projects-grid-tech">
-                    <p style={{ color: "#000" }} >React, Node.js, Express, Spotify API</p>
+                    <p style={{ color: "#000" }} >{tech}</p>
                 </div>
                 <div className="projects-grid-buttons">
-                    <a href="https://github.com/Sebas021210" target="_blank" rel="noopener noreferrer">
+                    <a href={github} target="_blank" rel="noopener noreferrer">
                         <Icon path={mdiGithub} size={1.5} color="black" />
                     </a>
-                    <a href="https://github.com/Sebas021210" target="_blank" rel="noopener noreferrer">
+                    <a href={demo} target="_blank" rel="noopener noreferrer">
                         <Icon path={mdiOpenInNew} size={1.5} color="black" />
                     </a>
                 </div>
